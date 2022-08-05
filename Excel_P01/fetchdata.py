e4b3ad0752cfa6ac72f3cc8c1a7077a22f23613f
benzunum93 <86158920+benzunum93@users.noreply.github.com>
2022-08-05 14:29:25 -0500

    Update fetchdata.py
    Second commit 05/08/22

--- Excel_P01/fetchdata.py
+++ Excel_P01/fetchdata.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import re
 import sqlite3
 import pandas as pd
 from datetime import datetime
@@ -8,7 +9,9 @@ import write_excel
 
 def horas_pilotos(lista_nombres,columna, cur):
     """
-    |Fetch for hour of each person
+    |Count how many hours a person has flight the simulator
+    |and create a report in excel with the name, and the hours
+    |in each position
     """
     horas=0
   
@@ -29,7 +32,8 @@ def horas_pilotos(lista_nombres,columna, cur):
 
 def contar_personas(datos,columna:int,num_person:dict)-> dict:
     """
-    |Calle by the function 'fechas' Count people are in the desired column a
+    |Called by the function 'fechas' to count the people that are in the desired column
+    
     """
     
     for row in datos:
@@ -48,14 +52,25 @@ def contar_personas(datos,columna:int,num_person:dict)-> dict:
 
 def fechas(date_ini:str,date_final:str, Fuerza:str,cur):
     """
-    |Fetch data for given dates
+    |Send the input data for given dates, then counts how many people are in a given date
+    |passing to the function 'contar_personas' the data in the database, the number of 
+    |the column to be search for the information and a varible 'num_personas' that will storage 
+    |the count of people.
+    |The made a report to excel with the names and the count of people
+    - cur -> is the cursor from the database
+    - Fuerza -> str
+    - num_personas -> int, count the people
+    
     """    
 
-    #Select the table name "Entrenamientos" and the column "Fecha"
-    #And find the values Between "date_ini" and "date_final"
+  
     
     Fuerza=Fuerza.lower()
     
+    #Select the table name "Entrenamientos" and the column "Fecha"
+    #and search the people Between "date_ini" and "date_final", and
+    #respectively force
+        
     cur.execute("SELECT * FROM Entrenamientos Where Fecha BETWEEN ? And ? And Fuerza=? ",(date_ini,date_final,Fuerza))
     rows = cur.fetchall()
     
@@ -72,13 +87,80 @@ def fechas(date_ini:str,date_final:str, Fuerza:str,cur):
     #Write a reporter in excel with the fetchdata
     write_excel.conteo_personas(num_personas, Fuerza)
 
+def reporte_horas_fac(date_ini:str,date_final:str,cur):
+    """
+    |Fetch data from the Fuerza 'Fac' between two dates
+    - cur -> is the cursor from the database
+    """
+    cur.execute("SELECT * FROM Entrenamientos Where Fecha BETWEEN ? And ? And Fuerza=? ",(date_ini,date_final,'fac'))
+    rows = cur.fetchall()
+    write_excel.reporte_fac(rows)
+    
+
+def reporte_horas_ejc(date_ini:str,date_final:str,cur):
+    """
+    |Fetch data from the Fuerza 'Ejc' between two dates
+    - cur -> is the cursor from the database
+    """
+    cur.execute("SELECT * FROM Entrenamientos Where Fecha BETWEEN ? And ? And Fuerza=? ",(date_ini,date_final,'ejc'))
+    rows = cur.fetchall()
+    df=pd.DataFrame(rows)
+    df=df.iloc[:,1:22]
+    
+    df.columns=['Fecha','Grado1','Instructor','Grado2','Piloto','Grado3',
+        'CoPiloto','Grado4','Observador','Hora_Entrada','Hora_Salida',
+        'Horas','Calificacion','VFR','IFR','NVR','Observaciones',
+        'Tipoentrenamiento', 'Fuerza','Tipo','Unidad']
+    for i in range(len(df['Fecha'])):
+        fecha=df['Fecha'][i]
+        fecha=fecha.split()
+        df['Fecha'][i]=fecha[0]
+        
+    
+    for n in range(len(df)):
+        hora1=df['Hora_Entrada'][n]
+        hora1=hora1[:5]
+        df['Hora_Entrada'][n]=hora1
+        
+    
+
+    for f in range(len(df)):
+        hora2=df['Hora_Salida'][f]
+        hora2=hora2[:5]
+        df['Hora_Salida'][f]=hora2
+    
+        
+    
+    hospedaje=pd.Series([])
     
     
+    for i in range(len(df)):
+        if df['Tipo'][i]=='A':
+            hospedaje[i]='SI'
+        else:
+            hospedaje[i]='NO'
+    df.insert(3, 'Hospedaje', hospedaje)
+    df.insert(6, 'Hospedaje2', hospedaje)
+    df.insert(9, 'Hospedaje3', hospedaje)
+    df.insert(12, 'Hospedaje4', hospedaje)
+    
+    write_excel.reporte_ejc(df)
+    
+
+    
+
 def fetch_main(data,reporte):
-    try:
-        print(data)
-    except:
-        pass
+    """
+    |Get all the inputs from the user and is storage in a
+    |Dictionary named 'data', also read what kind of report
+    |is needed, and send a query to the DB to retrive the information
+    |needed.
+    -Data   -> dict()
+    -reporte-> int
+    -Database-> Sqlite
+    
+    """
+    
     try:
         #Connection to the DB
         name_db='EntrenamientosDB.sqlite'    
@@ -88,8 +170,9 @@ def fetch_main(data,reporte):
 
             cur = conn.cursor()
             
-            
+            #Reporte de horas Voladas
             if reporte==1:
+                print('Generando reporte de horas de ', data['nombre'])
                 total=dict()
                 total['Instructor']=horas_pilotos(data['nombre'],"Instructor",cur)
                 total['Piloto']=horas_pilotos(data['nombre'],"Piloto",cur)
@@ -101,11 +184,19 @@ def fetch_main(data,reporte):
                         total['Total']=total['Total']+total[values]
 
                 write_excel.horas_pilotos(data['nombre'],total)
-            
+            #Reporte de conteo de personas entrenadas en fecha especifica
             elif reporte==2:
-                fechas(data['fecha_inicial'],data['fecha_final'],data['fuerza'],cur)
-                
+                print('Generando reporte Conteo de personas...')
+                fechas(data['fecha_inicial'],data['fecha_final'],data['Fuerza'],cur)
+            #Reporte mensual EJC
+            elif reporte==3:
+                print('Generando reporte EJC...')
+                reporte_horas_ejc(data['fecha_inicial'],data['fecha_final'],cur)
             
+            #Reporte mensual FAC
+            elif reporte==4:
+                print('Generando reporte FAC...')
+                reporte_horas_fac(data['fecha_inicial'],data['fecha_final'],cur)
             
             
     
@@ -116,26 +207,32 @@ def fetch_main(data,reporte):
         print(f'OSError database dont exists')
         raise RuntimeError from None 
 
-""" if __name__=='__main__':
-
-
+if __name__=='__main__':
     ###################################################
-    #This is the data that is fetch from the web and to be get from another
-    #proyect
-    lista=['melgarejo mauricio','Arango Juan','']
-    #Tipo de reporte
-    reporte=1
+    #This is the data that is need to be fetch from the web
+    # all the get data is storage in a ditionary and then it
+    #query the DB
+    
+    #Tipo de reporte, 1 conteo de horas, 2 conteo de personas, 3 reporte ejc,4 reporte fac
+    reporte=3
+
+    data=dict()
+    data['nombre']='melgarejo mauricio'
+    
                 #Year, Month, Day
-    date_ini=   datetime(2022,2,2)
-    date_final= datetime(2022,2,5)
+    try:
 
-    #date_ini= '-'.join(date_ini)
-    #date_final= '-'.join(date_final)
+        data['fecha_inicial']=datetime(2022,7,1)
+    except ValueError as e:
+        print(f'Values Error {e}')
+    try:
+        data['fecha_final']= datetime(2022,7,31)
+    except ValueError as e:
+        print(f'Values Error {e}')
+    
+    data['Fuerza']='ejc'
 
-    Fuerza='ejc'
     #######################################################
-    #Count hours of each pilot
-    fetch_main(lista,reporte)
-    #Count people in a given frametime
- """
+    #Send a query to be fetch in a DB
+    fetch_main(data,reporte)
     
